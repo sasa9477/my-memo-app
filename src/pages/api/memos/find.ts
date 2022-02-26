e1356b8e08d0fdd0ff952cc2419909d650e2c7b0
sasa9477 <sasa9477@outlook.com>
2022-02-26 19:05:21 +0900

    update find.ts

--- src/pages/api/memos/find.ts
+++ src/pages/api/memos/find.ts
@@ -2,7 +2,7 @@ import { NextApiHandler } from "next";
 import prismaApiClient from "./../prismaApiClient";
 
 const handler: NextApiHandler = async (req, res) => {
-    const id = req.body.id;
+    const id = req.query.memoId as string
 
     const foundMemo = await prismaApiClient.memo.findFirst({ where: { id: id }})
 
