3117284dc9215a8b68a69f2e42cabcc6cc930c85
sasa9477 <sasa9477@outlook.com>
2022-03-09 12:59:29 +0900

    axios api の baseURLを本番環境に変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production"
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    serverURL: isProduction ? "https://my-memo-app-ptm8qm1p5-sasa9477.vercel.app/" : "http://localhost:3000/"
+  }
 }
 
 module.exports = nextConfig
--- src/lib/axiosApiClient.ts
+++ src/lib/axiosApiClient.ts
@@ -2,8 +2,15 @@ import aspida from "@aspida/axios";
 import axios from "axios";
 import api from "../api/$api";
 
+const baseURL = (() => {
+    if (process.env.serverURL) {
+        return `${process.env.serverURL}/api/`
+    }
+    return "http://localhost:3000/api/"
+})()
+
 const axiosClient = axios.create({
-    baseURL: "http://localhost:3000/api/"
+    baseURL: baseURL
 })
 
 const axiosApiClient = api(aspida(axiosClient))
