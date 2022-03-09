331d6aae1ecf248effb5f0e5af917474b5a03053
sasa9477 <sasa9477@outlook.com>
2022-03-10 00:38:51 +0900

    change nextconfigjs

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production"
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    serverURL: isProduction ? "https://my-memo-app-nejdb76yx-sasa9477.vercel.app/" : "http://localhost:3000/"
+    serverURL: isProduction ? process.env.VERCEL_URL : "http://localhost:3000/"
   }
 }
 
