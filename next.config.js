d2367be9d6f04ceefddb2d4433ab68aa25e7363f
sasa9477 <sasa9477@outlook.com>
2022-03-10 00:05:43 +0900

    add nextconfig log

--- next.config.js
+++ next.config.js
@@ -5,8 +5,10 @@ const isProduction = process.env.NODE_ENV === "production"
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    serverURL: isProduction ? "https://my-memo-app-ptm8qm1p5-sasa9477.vercel.app/" : "http://localhost:3000/"
+    serverURL: isProduction ? "https://my-memo-app-nejdb76yx-sasa9477.vercel.app/" : "http://localhost:3000/"
   }
 }
+
+console.log('nextConfig :', nextConfig)
 
 module.exports = nextConfig
