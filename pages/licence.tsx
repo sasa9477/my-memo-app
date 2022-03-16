ae537ce17231300571c1137695b566608526e412
sasa9477 <sasa9477@outlook.com>
2022-03-16 18:40:10 +0900

    ライセンスページを整形して出力するように修正

--- pages/licence.tsx
+++ pages/licence.tsx
@@ -1,6 +1,6 @@
 import { List, ListItem, ListItemText } from '@mui/material';
 import licences from './../licenses.json';
-import packagejson from './../package.json';
+import packageJson from './../package.json';
 
 const Licence = (): JSX.Element => {
   const licenseKeys = Object.keys(licences)
@@ -8,17 +8,18 @@ const Licence = (): JSX.Element => {
   return (
     <List>
       <ListItem key='version'>
-        <ListItemText primary={`バージョン ${packagejson.version}`}/>
+        <ListItemText primary={`バージョン ${packageJson.version}`}/>
       </ListItem>
       {licenseKeys.map((key) => {
         const license = licences[key].licenses;
-        const copyright = licences[key].copylight ?? '';
+        const copyright = licences[key].copylight ?? `${license} License`;
         const licenseText = licences[key].licenseText;
         return (
           <ListItem key={key}>
           <ListItemText
             primary={key}
-            secondary={`${license} ${copyright} ${licenseText}`}/>
+            secondary={`${copyright}\n${licenseText}`}
+            secondaryTypographyProps={{ style: { whiteSpace: 'pre-wrap' } }}/>
         </ListItem>
         )
       })}
