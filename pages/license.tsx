050229c2dacf01ba82d6dd915df54c0df9126327
sasa9477 <sasa9477@outlook.com>
2022-03-19 23:13:42 +0900

    ライセンス画面のロード処理を画面表示後に修正

--- pages/license.tsx
+++ pages/license.tsx
@@ -1,7 +1,7 @@
-import { List, ListItem, ListItemText } from '@mui/material';
+import { List, ListItem, ListItemText, Typography } from '@mui/material';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
-import licenses from '../licenses.json';
+import { useEffect, useState } from 'react';
 import packageJson from '../package.json';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -21,14 +21,24 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 }
 
 const License = (): JSX.Element => {
+  const [ licenses, setLicenses ] = useState({})
   const licenseKeys = Object.keys(licenses)
+
+  useEffect(() => {
+    import('../licenses.json').then(value => setLicenses(value))
+  },[])
 
   return (
     <List>
       <ListItem key='version'>
         <ListItemText primary={`バージョン ${packageJson.version}`}/>
       </ListItem>
-      {licenseKeys.map((key) => {
+      {licenseKeys.length === 0 && (
+        <ListItem key='loading-licenses'>
+          <Typography>loading licenses...</Typography>
+        </ListItem>
+      )}
+      {licenseKeys.length > 0 && licenseKeys.map((key) => {
         const license = licenses[key].licenses;
         const copyright = licenses[key].copylight ?? `${license} License`;
         const licenseText = licenses[key].licenseText;
