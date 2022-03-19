a2b23062359a23a43d389ee8ffaffa7c71d3211a
sasa9477 <sasa9477@outlook.com>
2022-03-19 16:23:34 +0900

    meta tagに日本語コンテンツであることを表示

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from '@mui/material';
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from 'next/app';
-import CommonMeta from '../components/CommonMeta';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 import theme from '../themes/theme';
@@ -10,7 +9,6 @@ function MyApp({ Component, pageProps:{ session, ...pageProps }}: AppProps) {
   return (
     <SessionProvider session={session}>
       <ThemeProvider theme={theme}>
-        <CommonMeta/>
         <Layout>
           <Component {...pageProps} />
         </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default class MyDocument extends NextDocument {
     return (
       <Html>
         <Head>
+          {/* favicons */}
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
@@ -12,6 +13,14 @@ export default class MyDocument extends NextDocument {
           <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5"/>
           <meta name="msapplication-TileColor" content="#2b5797"/>
           <meta name="theme-color" content="#ffffff"/>
+
+          {/* noindex:   Webページをインデックスに登録しない */}
+          {/* nofollow:  クロールの際にページからのリンクを追跡しない */}
+          {/* noarchive: 検索結果にページへのキャッシュリンクを表示しない */}
+          <meta name="robots" content="noindex, nofollow, noarchive"/>
+
+          {/* 自動翻訳を表示しない */}
+          <meta httpEquiv="content-language" content="ja"/>
         </Head>
         <body>
           <Main/>
