6b55b94419333a1113e7be0942be233bd4ad9f09
sasa9477 <sasa9477@outlook.com>
2022-03-28 21:02:30 +0900

    メニュードロワーの実装

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Box, styled } from "@mui/material"
-import { ReactNode } from "react"
+import { ReactNode, useCallback, useState } from "react"
 import MyAppBar from "./MyAppBar"
 import SideBar from "./SideBar"
 
--- src/components/MyAppBar.tsx
+++ src/components/MyAppBar.tsx
@@ -1,5 +1,8 @@
-import { AppBar, Box, IconButton, styled, Toolbar } from "@mui/material"
+import { AppBar, Box, Drawer, IconButton, ListItemIcon, ListItemText, MenuItem, MenuList, styled, Toolbar } from "@mui/material"
+import Link from "next/link"
+import { Dispatch, SetStateAction, useState } from "react"
 import { AccountCircleIcon } from "./icons/AccountCircleIcon"
+import { EditIcon } from "./icons/EditIcon"
 import { MenuIcon } from "./icons/MenuIcon"
 import { SearchIcon } from "./icons/SearchIcon"
 import SearchBar from "./SearchBar"
@@ -8,6 +11,8 @@ type MyAppBarProps = {
 }
 
 const MyAppBar: React.FC<MyAppBarProps> = (): JSX.Element => {
+  const [ openMenu, setOpenMenu ] = useState(false)
+
   const StyledAppBar = styled(AppBar)(({ theme }) => ({
     position: 'fixed',
     top: 'auto',
@@ -26,14 +31,43 @@ const MyAppBar: React.FC<MyAppBarProps> = (): JSX.Element => {
   })
 
  return (
+  <>
    <StyledAppBar>
      <StyledToolbar>
-      <StyledIconButton>
+      <StyledIconButton onClick={() => setOpenMenu(true)}>
         <MenuIcon/>
       </StyledIconButton>
       <SearchBar/>
      </StyledToolbar>
    </StyledAppBar>
+   <Drawer
+    anchor="bottom"
+    open={openMenu}
+    onClose={() => setOpenMenu(false)}>
+    <MenuList>
+      <Link href={'/'} passHref>
+        <MenuItem key="index-icon-button" onClick={() => setOpenMenu(false)}>
+          <ListItemIcon>
+            <EditIcon/>
+          </ListItemIcon>
+          <ListItemText>
+            メモ
+          </ListItemText>
+        </MenuItem>
+      </Link>
+      <Link href={'/profile'} passHref>
+        <MenuItem key="profile-icon-button" onClick={() => setOpenMenu(false)}>
+          <ListItemIcon>
+            <AccountCircleIcon/>
+          </ListItemIcon>
+          <ListItemText>
+            プロフィール
+          </ListItemText>
+        </MenuItem>
+      </Link>
+    </MenuList>
+  </Drawer>
+  </>
  )
 }
 
