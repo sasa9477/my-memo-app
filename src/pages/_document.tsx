ab3460bac11ed73e81d57df7b1d735d96f5f091d
sasa9477 <sasa9477@outlook.com>
2022-04-11 01:29:50 +0900

    apple-touch-iconの設定を変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends NextDocument {
       <Html>
         <Head>
           {/* favicons */}
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
           <link rel="manifest" href="/favicons/site.webmanifest" crossOrigin="use-credentials" />
